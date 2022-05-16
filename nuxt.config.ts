775e71bca6bffeb76d06d9b7febc63316040775f
Jose Vega <jocvegar@gmail.com>
2022-05-16 09:15:55 -0600

    add fontawesome

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,7 +12,17 @@ export default defineNuxtConfig({
         { name: "description", content: "Review de restaurantes" },
         { charset: "utf-8" },
       ],
-      link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+      link: [
+        { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+        {
+          rel: "stylesheet",
+          href: "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css",
+          integrity:
+            "sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==",
+          crossorigin: "anonymous",
+          referrerpolicy: "no-referrer",
+        },
+      ],
     },
   },
   modules: ["@nuxtjs/tailwindcss"],
